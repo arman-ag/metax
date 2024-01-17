c502bc51f88a67dbab97f6b06c029d955d64cc48
ArmanAg <arman.alighanbary@gmail.com>
2024-01-17 15:40:37 +0330

    edit nextAuth url

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     env: {
         baseUrl: "http://172.16.16.127:8001",
         JWT_SECRET: "tTSSUYgnEkm6ioOt3Qe77h / xxcYMxs / NZ6iAtbMy83I=",
-        NEXTAUTH_URL: 'http://172.16.60.162:3000/'
+        NEXTAUTH_URL: 'http://172.16.16.127:3000/'
     },
     async redirects() {
         return [
